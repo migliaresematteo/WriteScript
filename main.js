5d2caca46c82072bde7eb5d4f0a5a2094d526b00
Matteo Migliarese <96019682+migliaresematteo@users.noreply.github.com>
2023-11-18 14:49:11 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,36 @@
-let element = document.querySelector('[data-write="true"]');
-let elementContent = element.innerHTML;
-let index = 0;
+let elements = document.querySelectorAll('[data-textScript="write"]');
 
-element.innerHTML = '';
+function handleWriteEffect(element) {
+    let elementContent = element.innerHTML;
+    let index = 0;
 
-let writeSpeed = parseInt(element.getAttribute('data-write-speed')) || 1000;
+    element.innerHTML = '';
+
+    let writeSpeed = parseInt(element.getAttribute('data-write-speed')) || 1000;
 
     function iterate() {
-    let intervallo = setInterval(() => {
-        if (index < elementContent.length) {
-        element.innerHTML += elementContent[index];
-        index++;
-        } else {
-        clearInterval(intervallo);
-        }
-    }, writeSpeed);
+        let intervallo = setInterval(() => {
+            if (index < elementContent.length) {
+                element.innerHTML += elementContent[index];
+                index++;
+            } else {
+                clearInterval(intervallo);
+            }
+        }, writeSpeed);
     }
 
-iterate();
+    iterate();
+}
+
+let observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            handleWriteEffect(entry.target);
+            observer.unobserve(entry.target);
+        }
+    });
+}, { threshold: 0.5 });
+
+elements.forEach(element => {
+    observer.observe(element);
+});
